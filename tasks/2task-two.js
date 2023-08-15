2ec7d826e163baf5a539af1d8373ddbfe82a11d7
donvico <wvictoryc@gmail.com>
2023-08-15 16:15:42 +0100

    commit

--- tasks/2task-two.js
+++ tasks/2task-two.js
@@ -1,30 +1,23 @@
-// function compare (a,b){
-//     for(let i = 0; i< a.length; i++){
-//         let isPresent = false
-//         for(let j =0; j< b.length; j++){
-//             isPresent = true
-//             if(a.charAt(a[i] === b.charAt(b[j]))){
-//                 break
-//             }
-//            if (isPresent === false){
-//             break
-//            }
-//         }
-//     }
-// }
-// console.log(compare('tunde', 'ednut'))
-// function myName (fullname){
-//     let nameArr = fullname.Split('')
-// }
-// myName()
-function isAnagram (word1){
- let conv1 = word1.split('').reverse().join('')
-//  let conv2 = word2.split('').reverse().join()
- if(word1 === conv1){
-    return true
- }
- else{
-    return false
- }
+async function getLocation() {
+    try {
+        const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+        console.log(position);
+
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`);
+        const data = await response.json();
+
+        document.getElementById('city').textContent = data.address.city;
+        document.getElementById('country').textContent = data.address.country;
+        document.getElementById('time').textContent = new Date().toLocaleTimeString();
+        document.getElementById('altitude').textContent = position.coords.altitude;
+    } catch (error) {
+        console.error('Error fetching location:', error.message);
+    }
 }
-console.log(isAnagram('racecar'))
+
+getLocation();
